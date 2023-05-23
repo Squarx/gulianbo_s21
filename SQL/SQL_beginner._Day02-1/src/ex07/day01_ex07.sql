5c9f4d51130aa8ed389958b64b0ad2b9b2d017d3
DimasX <gulianbo@student.21-school.ru>
2023-05-23 23:18:05 +0300

    Day02 fix 7-8

--- SQL/SQL_beginner._Day02-1/src/ex07/day01_ex07.sql
+++ SQL/SQL_beginner._Day02-1/src/ex07/day01_ex07.sql
@@ -1,4 +1,4 @@
-select pizza_name
+select pz.name as pizzeria_name
 from person_visits
          inner join person p on p.id = person_visits.person_id
          inner join pizzeria pz on pz.id = person_visits.pizzeria_id
