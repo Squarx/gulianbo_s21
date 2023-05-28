b6d606eac8bce88d46eee41cdf759205322883b7
Squarx <dimasclashx@gmail.com>
2023-05-28 20:14:22 +0300

    Day 07 v1

--- SQL/SQL_beginner._Day07-1/src/ex09/day07_ex09.sql
+++ SQL/SQL_beginner._Day07-1/src/ex09/day07_ex09.sql
@@ -1,14 +1,14 @@
-SELECT
-  p.address AS address,
-  ROUND(MAX(age::numeric) - (MIN(age::numeric) / MAX(age::numeric)), 2) AS formula,
-  ROUND(AVG(age), 2) AS average,
-  CASE
-    WHEN ROUND(MAX(age::numeric) - (MIN(age::numeric) / MAX(age::numeric)), 2) > ROUND(AVG(age), 2) THEN true
-    ELSE false
-  END AS comparison
-FROM
-  person p
-GROUP BY
-  p.address
-ORDER BY
-  p.address;
+WITH adress_n_ages as (SELECT address,
+                              ROUND(MAX(age::NUMERIC) - MIN(age::NUMERIC) / MAX(age::NUMERIC), 2) AS formula,
+                              ROUND(AVG(age), 2)                                                  AS average
+                       FROM person
+                       GROUP BY address
+                       ORDER BY address)
+SELECT address,
+       formula,
+       average,
+       CASE
+           WHEN formula > average THEN true
+           ELSE false
+           END AS comparison
+FROM adress_n_ages;
